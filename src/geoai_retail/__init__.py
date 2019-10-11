e7dab2035f78ed3b654ad6515a9081c992cb9e6f
Joel McCune <knu2xs@gmail.com>
2019-10-11 15:29:33 -0700

    Fix init import

--- src/geoai_retail/__init__.py
+++ src/geoai_retail/__init__.py
@@ -4,6 +4,6 @@ __author__ = 'Joel Mccune'
 __license__ = 'GNU AGPLv3'
 __copyright__ = 'Copyright 2019 by Joel McCune'
 
-import geoai_retail.proximity_local as proximity
+import geoai_retail.proximity as proximity
 import geoai_retail.utils as utils
 import geoai_retail.analysis as analysis
