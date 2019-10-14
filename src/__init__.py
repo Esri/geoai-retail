cac1a85f3015c523fdcf7007eacd2bfd41d60a93
Joel McCune <knu2xs@gmail.com>
2019-10-14 16:34:04 -0700

    moving resources around for packaging

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'geoai_retail'
+__version__ = '0.1.0'
+__author__ = 'Esri'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2019 by Esri'
+
+import geoai_retail.proximity as proximity
+import geoai_retail.utils as utils
+import geoai_retail.analysis as analysis
