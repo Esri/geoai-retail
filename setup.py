a3284999354f928cdcb08e554d038dd39b7329a6
Joel McCune <knu2xs@gmail.com>
2019-10-14 16:14:24 -0700

    updates for packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,30 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name='geoai_retail',
-    packages=find_packages(),
-    version='0.1.0',
-    description='A short description of the project.',
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+with open('requirements.txt') as reqs:
+    requirements = reqs.read().splitlines()
+
+setuptools.setup(
+    name='ba_data_paths',
+    version='0.6',
     author='Joel McCune',
-    license='GNU AGPLv3',
+    author_email='jmccune@esri.com',
+    description='Feature engineering using ArcGIS Pro with Business Analyst for using quantitative Geography '
+                'with Machine Learning.',
+    license='Apache 2.0',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/knu2xs/geoai-retail',
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Microsoft :: Windows',
+        'Topic :: Scientific/Engineering :: GIS'
+    ]
 )
