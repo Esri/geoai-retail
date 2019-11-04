f2063439cf52394250f3e2a1c38e8cc3528089db
Joel McCune <knu2xs@gmail.com>
2019-11-04 08:40:06 -0800

    Updates addressing environment

--- {{cookiecutter.project_name}}/create_env.cmd
+++ {{cookiecutter.project_name}}/create_env.cmd
@@ -1,5 +1,5 @@
 call conda create --name {{cookiecutter.conda_environment_name}} --clone arcgispro-py3
 call activate {{cookiecutter.conda_environment_name}}
-call conda env update -f ../environment.yml
+call conda env update -f environment.yml
 jupyter labextension install @jupyter-widgets/jupyterlab-manager -y
-jupyter labextension install arcgis-map-ipywidget@1.7 -y
+jupyter labextension install arcgis-map-ipywidget@1.7.0 -y
